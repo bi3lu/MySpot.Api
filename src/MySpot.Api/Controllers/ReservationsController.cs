dee5ff143bcdc262c0f8eb5610ca67d8c6c76429
Kuba Bielecki <kuba.bielecki@outlook.com>
2023-09-08 22:00:25 +0200

    Refactor. Code refactor by single responsibility principle - added service.

---
+++ src/MySpot.Api/Controllers/ReservationsController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySpot.Api.Models;
+using MySpot.Api.Services;
+
+namespace MySpot.Api.Controllers
+{
+    [ApiController]
+    [Route("reservations")]
+    public class ReservationsController : ControllerBase
+    {
+        private readonly ReservationsService _service = new();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Reservation>> Get() => Ok(_service.GetAll());
+
+        [HttpGet("{id:int}")]
+        public ActionResult<Reservation> Get([FromRoute] int id)
+        {
+            var reservation = _service.Get(id);
+
+            if (reservation is null)
+            {
+                return NotFound();      
+            }
+
+            return Ok(reservation);
+        }
+
+        [HttpPost]
+        public ActionResult Post(Reservation reservation)
+        {
+            var id = _service.Create(reservation);
+
+            if (id is null)
+            {
+                return BadRequest();
+            }
+
+            return CreatedAtAction(nameof(Get), new { id }, null);
+        }
+
+        [HttpPut("id:int")]
+        public ActionResult Put(int id, Reservation reservation)
+        {
+            reservation.Id = id;    
+           
+            if (_service.Update(reservation))
+            {
+                return NoContent();
+            }
+
+            return NotFound();
+        }
+
+        [HttpDelete("id:int")]
+        public ActionResult Delete(int id)
+        {
+            if (_service.Delete(id))
+            {
+                return NoContent();
+            }
+
+            return NotFound();
+        }
+    }
+}
---
+++ src/MySpot.Api/Services/ReservationsService.cs
@@ -0,0 +1,72 @@
+﻿using MySpot.Api.Models;
+
+namespace MySpot.Api.Services
+{
+    public class ReservationsService
+    {
+        private static int _id = 1;
+        private static readonly List<Reservation> _reservations = new();
+        private static readonly List<string> _parkingSpotNames = new()
+        {
+            "P1","P2", "P3", "P4", "P5"
+        };
+
+        public Reservation Get(int id)
+            =>  _reservations.SingleOrDefault(r => r.Id == id);
+
+        public IEnumerable<Reservation> GetAll() 
+            => _reservations;
+
+        public int? Create(Reservation reservation)
+        {
+            if (_parkingSpotNames.All(p => p != reservation.ParkingSpotName))
+            {
+                return default;
+            }
+
+            reservation.Date = DateTime.UtcNow.AddDays(1).Date;
+            var reservationAlreadyExists = _reservations.Any(r
+                => r.ParkingSpotName == reservation.ParkingSpotName
+                && r.Date == reservation.Date);
+
+            if (reservationAlreadyExists)
+            {
+                return default;
+            }
+
+            reservation.Id = _id;
+            _id++;
+            _reservations.Add(reservation);
+
+            return reservation.Id;
+        }
+
+        public bool Update(Reservation reservation)
+        {
+            var existingReservation = _reservations.SingleOrDefault(r => r.Id == reservation.Id);
+
+            if (existingReservation is null)
+            {
+                return false;
+            }
+
+            existingReservation.LicensePlate = reservation.LicensePlate;
+
+            return true;
+        }
+
+        public bool Delete(int id)
+        {
+            var existingReservation = _reservations.SingleOrDefault(r => r.Id == id);
+
+            if (existingReservation is null)
+            {
+                return false;
+            }
+
+            _reservations.Remove(existingReservation);
+
+            return true;
+        }
+    }
+}
